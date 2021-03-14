ade86cf0d03baa2d3b5cf4aed30f55df184bffdf
taomao <ruslan.akm@gmail.com>
2021-03-15 00:22:05 +0300

    fix config

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 [DICT]
-#Путь до файла словарей, если в этой же директории, то только имя словаря.
 path = /home/ruslan/Dropbox/Translate/fall_dict.csv 
 
 [TIMEFLASH]
-flash_timeout = 5 #Сколько картачка висит на экране.
-interval = 600 #Сколько времени между карточками.
+flash_timeout = 5
+interval = 600 
 
 [STACKWORDS]
-count = 50 #Сколько слов в наборе.
+count = 50
